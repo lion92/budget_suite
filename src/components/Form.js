c3cb72433bbef84773a4ad814e8a52161245e234
lion92 <38532569+lion92@users.noreply.github.com>
2025-02-10 13:44:13 +0100

    correction du reset de mofication une fois validé

--- src/components/Form.js
+++ src/components/Form.js
@@ -196,6 +196,7 @@ export default function Form(props) {
         fetchAPIupdate();
         setValue("");
         setTitre("");
+        setDescription("");
     };
     /////////////////////////
 
